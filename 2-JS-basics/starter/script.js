0e0f07fdc9b3c7522e2b1a60bad9d2bc718a0fd7
chemelson <ross03@mail.ru>
2020-04-07 10:54:10 +0300

    Finish JS basics

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -139,7 +139,7 @@ if (isMarried) {
     console.log(firstName + ' will hopefully marry soon');
 }
  */
-
+/*
 var heightJohn, heightMark, weightJohn, weightMark, bmiJohn, bmiMark;
 heightJohn = 1.95;
 heightMark = 1.69;
@@ -157,3 +157,169 @@ if (bmiMark > bmiJohn) {
 } else {
     console.log('John\'s BMI is higher');
 }
+
+*/
+
+/*
+ Boolean logic
+
+var firstName = 'John';
+var age = 16;
+if (age < 13) {
+    console.log(firstName + ' is a boy');
+} else if (age >= 13 && age < 20) {
+    console.log(firstName + ' is a teenager');
+} else if (age >=20 && age < 30) {
+    console.log(firstName + ' is a young man');
+} else {
+    console.log(firstName + ' is a man');
+}
+ */
+
+// Ternary operator
+/*
+var firstName = 'John';
+var age = 16;
+
+age >= 18 ? console.log("Can drink") : console.log("Drink cola");
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+if (age >= 18) {
+    var drink = 'beer';
+} else {
+    var drink = 'juice';
+}
+
+var job = 'teacher';
+switch (job) {
+    case 'teacher':
+    case 'instructor':
+        console.log(firstName + ' teaches kids how to code');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives an uber');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs');
+        break;
+    default:
+        console.log(firstName + ' do smth else');
+}
+*/
+/*
+var height;
+height = 23;
+
+if (height || height === 0) {
+    console.log('Variable is defined');
+} else {
+    console.log('Variable is not defined');
+}
+
+if (height === '23') {
+    console.log('The === does not type coercion');
+}
+*/
+/*
+function calculateAge(birthYear) {
+    return 2018 - birthYear;
+}
+
+var ageJohn = calculateAge(1986);
+var ageMike = calculateAge(1948);
+var ageMJane = calculateAge(1969);
+console.log(ageJohn, ageMike, ageMJane);
+*/
+
+// Arrays
+/*
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names);
+console.log(names.length);
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+
+var john = ['John', 'Smith', 1998, 'teacher', false];
+john.push('blue');
+john.unshift('Mr.');
+console.log(john);
+john.pop();
+john.shift();
+john.indexOf(1998);
+console.log(john.indexOf(1998));
+console.log(john.indexOf('designer'));
+ */
+
+// Objects and properties
+/*
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false
+};
+console.log(john);
+console.log(john.firstName);
+var x = 'birthYear';
+console.log(john[x]);
+
+john.job = 'designer';
+john['isMarried'] = true;
+console.log(john);
+
+var jane = new Object();
+jane.name = 'Jane';
+jane.birthYear = 1969;
+jane['lastName'] = 'Smith';
+console.log(jane);
+ */
+
+// Methods
+/*
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function() {
+        this.age = 2018 - this.birthYear;
+    }
+};
+
+john.calcAge();
+console.log('John age: ' + john.age);
+*/
+
+// Loops and iteration
+for (var i = 1; i <= 10; i += 2) {
+    console.log(i);
+}
+
+var john = ['John', 'Smith', 1998, 'teacher', false];
+john.forEach();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
